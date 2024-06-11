65b1eae76216e3e16d694b74993cd6a829ddd32b
Rutchy <171798882+Rutchyy@users.noreply.github.com>
2024-06-11 17:38:51 +1000

    Well my git commits are already so bad, how could it get worse?
    Linked the operation function to the html with some typical DOM manipulation, and styled the calculator.

--- main.js
+++ main.js
@@ -1,43 +1,81 @@
+// The following function returns the evaluation of a string.
+// operate(1 + 2 * 3) Would return 9, it follows a left-to-right process.
 function operate(string) {
-    let numbers = string.split(" ");
+    let numbers = string.split(" ")
 
     numbers.map((item, index) => {
         if (item.match(/^[0-9]+$/)!= null) {
-            numbers[index] = Number(item);
+            numbers[index] = Number(item)
         }
-    });
+    })
 
-    console.log(numbers); // [ 141, '+', 12, '-', 2 ]
+    let total = numbers[0]
 
-    let total = numbers[0]; // 141
+    numbers.splice(0, 1)
 
-    numbers.splice(0, 1);
-
-    let shortOperation;
+    let shortOperation
 
     for (let i = 0; i <= 2; i++) {
-        shortOperation = numbers.splice(0, 2);
+        shortOperation = numbers.splice(0, 2)
 
         switch (shortOperation[0]) {
             case "+":
-                total += shortOperation[1];
+                total += shortOperation[1]
                 break;
             case "-":
-                total -= shortOperation[1];
+                total -= shortOperation[1]
                 break;
             case "*":
-                total *= shortOperation[1];
+                total *= shortOperation[1]
                 break;
             case "/":
-                total /= shortOperation[1];
-                break;
+                total /= shortOperation[1]
+                break
         }
     }
 
-    return total;
+    return total
 }
 
+let operation = ""
 
 const sample = "141 + 12 * 2"
 
-console.log(operate(sample))
+operate(sample)
+
+const container = document.querySelector(".container")
+const typeBox = container.querySelector(".input")
+const row = container.querySelector("button")
+
+container.addEventListener("click", (event) => {
+    let target = event.target
+
+    switch(target.className) {
+        case "num":
+            let number = target.id.substring(1)
+            operation += number
+            break
+        case "operator":
+            switch(target.id) {
+                case "add":
+                    operation += " + "
+                    break
+                case "subtract":
+                    operation += " - "
+                    break
+                case "multiply":
+                    operation += " * "
+                    break
+                case "divide":
+                    operation += " / "
+                    break
+            }
+            break
+        case "equal":
+            operation = operate(operation)
+    }
+
+    typeBox.textContent = operation
+
+    console.log(operation)
+})
